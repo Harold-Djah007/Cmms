6379e2c1e0020ed5637c76d2334661b0862fa261
Harold-Djah007 <146589784+Harold-Djah007@users.noreply.github.com>
2026-09-15 11:48:34 +0000

    Refresh offline cache for field-ready release

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'safimaint-v1';
+const CACHE = 'safimaint-v2-field-ready';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./assets/styles.css','./assets/app.js','./assets/favicon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
